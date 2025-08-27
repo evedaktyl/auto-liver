ec90d1c2379fe72995068bfb50ba9e9cd7343355
evedaktyl <evedaktyl@gmail.com>
2025-08-27 11:23:20 +0800

    Add routes frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Upload from './pages/Upload.jsx'
 import Scans from './pages/Scans.jsx'
 import ScanDetail from './pages/ScanDetail.jsx'
+import Drafts from './pages/Drafts.jsx'
+import DraftDetail from './pages/DraftDetail.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
@@ -13,6 +15,8 @@ createRoot(document.getElementById('root')).render(
       <Route path="/upload" element={<Upload />}/>
       <Route path="/scans" element={<Scans />}/>
       <Route path="/scans/:scanId" element={<ScanDetail />}/>
+      <Route path="/drafts" element={<Drafts />}/>
+      <Route path="/drafts/:draftId" element={<DraftDetail />} />
     </Routes>
     </BrowserRouter>
   </StrictMode>
