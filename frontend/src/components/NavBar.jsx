35bb808e3c9e7a352147a7da7368e70e37469366
evedaktyl <evedaktyl@gmail.com>
2025-09-15 14:28:37 +0800

    Add simple NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,23 @@
+import { Link, NavLink } from 'react-router-dom'
+
 export default function NavBar() {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <div className='flex justify-between items-center'> 
+            <div className='gap-x-4'>
+                Auto-liver
+                <NavLink to={"/upload"} className={({ isActive }) => isActive ? "text-[#8cd3d5]" : "text-gray-200"}>
+                    Upload
+                </NavLink>
+                <NavLink to={"/drafts"} className={({ isActive }) => isActive ? "text-[#8cd3d5]" : "text-gray-200"}>
+                    Drafts
+                </NavLink>
+                <NavLink to={"/scans"} className={({ isActive }) => isActive ? "text-[#8cd3d5]" : "text-gray-200"}>
+                    Scans
+                </NavLink>
+            </div>
+            <div>Username</div>
+            </div>
+        </nav>
     );
 }
