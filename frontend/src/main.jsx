74c55976ecc3b5303863ffce1172679a902c26a4
evedaktyl <evedaktyl@gmail.com>
2025-09-15 16:33:56 +0800

    Add NavBar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,13 @@ import Scans from './pages/Scans.jsx'
 import ScanDetail from './pages/ScanDetail.jsx'
 import Drafts from './pages/Drafts.jsx'
 import DraftDetail from './pages/DraftDetail.jsx'
+import NavBar from './components/NavBar.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <NavBar/>
     <Routes>
       <Route path="/upload" element={<Upload />}/>
       <Route path="/scans" element={<Scans />}/>
