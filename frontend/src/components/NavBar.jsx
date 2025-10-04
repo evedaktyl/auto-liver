3d56a1fc37535eac504a25d70da1d90c7d2f5d95
evedaktyl <evedaktyl@gmail.com>
2025-10-04 15:36:39 +0800

    Add dark mode for NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,13 +7,13 @@ export default function NavBar() {
             <div className='flex justify-between items-center'> 
             <div className='gap-x-4'>
                 Auto-liver
-                <NavLink to={"/upload"} className={({ isActive }) => isActive ? "text-[#8cd3d5]" : "text-gray-200"}>
+                <NavLink to={"/upload"} className={({ isActive }) => isActive ? "text-[#8cd3d5]" : "text-gray-900 dark:text-gray-200"}>
                     Upload
                 </NavLink>
-                <NavLink to={"/drafts"} className={({ isActive }) => isActive ? "text-[#8cd3d5]" : "text-gray-200"}>
+                <NavLink to={"/drafts"} className={({ isActive }) => isActive ? "text-[#8cd3d5]" : "text-gray-900 dark:text-gray-200"}>
                     Drafts
                 </NavLink>
-                <NavLink to={"/scans"} className={({ isActive }) => isActive ? "text-[#8cd3d5]" : "text-gray-200"}>
+                <NavLink to={"/scans"} className={({ isActive }) => isActive ? "text-[#8cd3d5]" : "text-gray-900 dark:text-gray-200"}>
                     Scans
                 </NavLink>
             </div>
